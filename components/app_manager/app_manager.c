1c16a69218e1147a4677ded6ea52db59ee2140d6
Trần Mạnh <manhtran98bg@gmail.com>
2025-11-22 16:16:20 +0700

    commit

--- components/app_manager/app_manager.c
+++ components/app_manager/app_manager.c
@@ -6,7 +6,7 @@
 #include "bsp/esp-bsp.h"
 
 #include "esp_timer.h"
-
+#include "ui.h"
 static const char *TAG = "app_manager";
 
 static void gif_task(void *pvParameters)
@@ -14,15 +14,6 @@ static void gif_task(void *pvParameters)
     int delayMs = 0;
     while (true)
     {
-        // if (esp_GIF_openFile("/littlefs/gif/1.gif"))
-        // {
-        //     int64_t t0 = esp_timer_get_time();
-        // 	while (esp_GIF_playFrame(&delayMs, NULL)) {
-        //     }
-        //     int64_t t1 = esp_timer_get_time();
-        //     ESP_LOGI("GIF", "Decode+draw = %lld us", t1 - t0);
-        // 	esp_GIF_close();
-        // }
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
@@ -35,9 +26,11 @@ esp_err_t app_manager_init(void)
     bsp_littlefs_mount();
     /* Wait until other tasks finish screen operations */
     bsp_display_lock(0);
-    lv_obj_t *screen = lv_disp_get_scr_act(display);
-    lv_obj_set_style_bg_color(screen, lv_color_make(0, 0, 0), LV_PART_MAIN);
-    lv_obj_set_style_bg_opa(screen, LV_OPA_COVER, LV_PART_MAIN);
+    ui_init();
+    // lv_obj_t *screen = lv_disp_get_scr_act(display);
+    // lv_obj_set_style_bg_color(screen, lv_palette_main(LV_PALETTE_BLUE), LV_PART_MAIN);
+    // lv_obj_set_style_bg_color(screen, lv_color_make(0, 0, 0), LV_PART_MAIN);
+    // lv_obj_set_style_bg_opa(screen, LV_OPA_COVER, LV_PART_MAIN);
 
     // create gif
     // lv_obj_t *gif;
@@ -46,15 +39,25 @@ esp_err_t app_manager_init(void)
     // lv_gif_set_src(gif, "A:/littlefs/gif/turn_right.gif");
     // lv_obj_center(gif);
 
-    // lv_obj_t * lottie = lv_rlottie_create_from_file(screen, 30, 30, "/littlefs/lottie/turn_right.json");
-    lv_obj_t * lottie = lv_lottie_create(lv_screen_active());
+    lv_obj_t *lottie = lv_lottie_create(lv_screen_active());
     lv_lottie_set_src_file(lottie, "/littlefs/lottie/turn_right.json");
-    static uint8_t buf[60 * 60 * 4];
-    lv_lottie_set_buffer(lottie, 60, 60, buf);
+    static uint8_t buf[30 * 30 * 4];
+    lv_lottie_set_buffer(lottie, 30, 30, buf);
+
+    Tvg_Matrix matrix = {
+        -1, 0, 60,
+        0, 1, 0,
+        0, 0, 1};
+
+    tvg_paint_set_transform(((lv_lottie_t *)lottie)->tvg_paint, &matrix);
+    lv_anim_t *anim = lv_lottie_get_anim(lottie);
+    lv_anim_set_duration(anim, 1050);
+    lv_anim_set_reverse_delay(anim, 100);
+    lv_anim_set_reverse_duration(anim, 1050);
     lv_obj_center(lottie);
-    /* Load the screen */
-    // xTaskCreate(gif_task, "gif_task", 8192, NULL, configMAX_PRIORITIES - 1, NULL);
-    lv_scr_load(screen);
+    // /* Load the screen */
+
+    // lv_scr_load(screen);
     bsp_display_unlock();
 
     ESP_LOGI(TAG, "Initialized successfully.");
